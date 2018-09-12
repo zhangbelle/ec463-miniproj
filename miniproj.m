aa5b9baf599b72a6a56ddfb031c2da2b1a84be81
aleung19 <43117758+aleung19@users.noreply.github.com>
2018-09-11 20:48:17 -0400

    Update miniproj.m

--- miniproj.m
+++ miniproj.m
@@ -1,4 +1,16 @@
-function cardetect
-foregroundDetector = vision.ForegroundDetector('NumGaussians', 3, ...
-    'NumTrainingFrames', 50);
-end
+% function numcars = cardetext(v)
+v = vision.VideoFileReader('carfootage.mp4'); % read footage
+detector = vision.ForegroundDetector('NumGaussians', 3, ...
+    'NumTrainingFrames', 100); % creates foreground mask
+blob = vision.BlobAnalysis('AreaOutputPort', false, ...
+     'BoundingBoxOutputPort', true, 'CentroidOutputPort', false, ...
+     'MinimumBlobArea', 70); % analyzes connected regions in the mask
+numcars = 0; % initialize counter
+while ~isDone(v)
+    frame = v();
+    foreground = detector(frame);
+    bbox = blob(foreground);
+    % step function somewhere?
+end % process video
+% Detecting code, NEED TO CLEAN NOISE!
+% Car counter
