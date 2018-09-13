de872a9e8268cd95bfe529b7c64a75b7bac44b5d
aleung19 <43117758+aleung19@users.noreply.github.com>
2018-09-13 10:35:04 -0400

    Update miniproj.m

--- miniproj.m
+++ miniproj.m
@@ -3,14 +3,24 @@ v = vision.VideoFileReader('carfootage.mp4'); % read footage
 detector = vision.ForegroundDetector('NumGaussians', 3, ...
     'NumTrainingFrames', 100); % creates foreground mask
 blob = vision.BlobAnalysis('AreaOutputPort', false, ...
-     'BoundingBoxOutputPort', true, 'CentroidOutputPort', false, ...
+     'BoundingBoxOutputPort', true, 'CentroidOutputPort', true, ...
      'MinimumBlobArea', 70); % analyzes connected regions in the mask
 numcars = 0; % initialize counter
+videoPlayer = vision.VideoPlayer();
 while ~isDone(v)
     frame = v();
     foreground = detector(frame);
-    bbox = blob(foreground);
+%     bbox = blob(foreground);
+    [CENTROID, BBOX] = step(blob, foreground);
+    shapeInserter = vision.ShapeInserter('BorderColor', 'White');
+    out = shapeInserter(frame, BBOX);
+    videoPlayer(out);
     % step function somewhere?
-end % process video
-% Detecting code, NEED TO CLEAN NOISE!
-% Car counter
+end
+% Will need to adjust inputs from Raspberry Pi Camera
+% A lot of this code is just taken from the Matlab site so we're gonna
+% have to shake it up a bit to make it our own thing
+% NEED TO CLEAN NOISE FROM DETECTION!
+% Car counter, will utilize the centroid output from blob analysis since
+% that counts blob. Tracking blob's from creation to disappearance will
+% +1 counter
